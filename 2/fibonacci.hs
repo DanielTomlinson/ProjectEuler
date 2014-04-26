b2c6ce6f95962c77b00604444b163482eae98b95
Daniel Tomlinson <dan@tomlinson.io>
2014-04-26 07:48:26 +0100

    initial commit

---
+++ 2/fibonacci.hs
@@ -0,0 +1,16 @@
+module Main where
+
+  import Data.List
+
+  fibs :: [Integer]
+  fibs = 1 : 1 : zipWith (+) fibs (tail fibs)
+
+  fibsBelow :: Integer -> [Integer]
+  fibsBelow n = [ x | x <- takeWhile (<= n) fibs]
+
+  evenIntegers :: [Integer] -> [Integer]
+  evenIntegers il = filter even il
+
+  main :: IO ()
+  main = do
+    putStrLn $ show $ sum (evenIntegers (fibsBelow 4000000))

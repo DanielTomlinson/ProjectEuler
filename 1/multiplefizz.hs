8ba8c4f616434e8185622c0c94ee9ae974158a1b
Daniel Tomlinson <dan@tomlinson.io>
2014-04-26 07:53:12 +0100

    Use variable max

--- 1/multiplefizz.hs
+++ 1/multiplefizz.hs
@@ -2,9 +2,9 @@ module Main where
 
 import Data.List (union)
 
-doThing' = sum (union [3,6..999] [5,10..999])
-doThing = sum [x | x <- [1..999], x `mod` 3 == 0 || x `mod` 5 == 0]
+doThing' n = sum (union [3,6..n] [5,10..n])
+doThing n = sum [x | x <- [1..n], x `mod` 3 == 0 || x `mod` 5 == 0]
 
 main :: IO ()
 main = do   
-    putStrLn $ show doThing'
+    putStrLn $ show (doThing' 1000)
